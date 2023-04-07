64af3fe147647a78c5bc27497780347790cb248d
EC2 Default User <ec2-user@ip-172-31-21-246.ap-northeast-1.compute.internal>
2023-04-07 11:49:01 +0000

    [Update] 全体ページのレイアウトを調整

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Meshiterro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.middleware.delete ActionDispatch::HostAuthorization
 
     # Configuration for the application, engines, and railties goes here.
     #
